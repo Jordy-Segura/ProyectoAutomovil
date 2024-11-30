6edb49ee0ac9d9773374821ff476866f37335641
jordy <marcelo2005jmsp@gmail.com>
2024-11-30 16:38:55 -0500

    repositorio creado

---
+++ proyectoAutomovil/src/main/java/ec/edu/espoch/proyectoautomovil/ProyectoAutomovil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.proyectoautomovil;
+
+/**
+ *
+ * @author jordy
+ */
+public class ProyectoAutomovil {
+
+    public static void main(String[] args) {
+        
+    }
+}
